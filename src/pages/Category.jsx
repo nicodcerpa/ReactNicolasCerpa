4ba50cda8ce4d658c4164be30ac7c95f0cf51311
nicolas cerpa <nicolascerpa@icloud.com>
2023-11-23 23:45:01 -0300

    cambios

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import ItemListContainer from "../components/ItemListContainer/ItemListContainer";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+
 const Category = () => {
     const [products, setProducts] = useState ([]);
-  
+    const { categoryId } = useParams();
+
   useEffect(() => {
    axios
-   .get("https://dummyjson.com/products")
+   .get(`https://dummyjson.com/products/category/${categoryId}`)
    .then(res => {
     setProducts(res.data.products);
    })
    .catch((error) => console.log(error));
-  }, [])
+  }, [categoryId]);
+
+
+
+console.log(categoryId);
+
     return <ItemListContainer products={products} />;
     
 }
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "../pages/Home";
 import NavBarComponent from "../components/NavBarComponent/NavBarComponent";
 import Category from "../pages/Category";
+import Item from "../pages/Item";
 
 const MainRoutes = () => {
     return (
@@ -10,6 +11,7 @@ const MainRoutes = () => {
    <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/category/:categoryId" element={<Category />} />
+    <Route path="/item/:id" element={<Item />} />
    </Routes>
    </BrowserRouter>
     );
